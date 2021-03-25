4c07da92d1a1bf86d1a8338b85c2cc5caa6f5da5
Keionne Derousselle <knnderousselle@gmail.com>
2021-03-24 21:59:14 -0600

    adding reset scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,13 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'github-search',
+    title: 'GitHub Search',
     meta: [ { charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: '' } ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/validation.js'],
