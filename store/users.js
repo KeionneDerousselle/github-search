027c8bba3f42ece5d5e73d709e9f6120e101a92c
Keionne Derousselle <knnderousselle@gmail.com>
2021-03-26 16:30:14 -0600

    adding logic in the users store to handle search results from the api

--- store/users.js
+++ store/users.js
@@ -1,24 +1,35 @@
 export const state = () => ({
-  users: []
+  users: [],
+  resultsPerPage: 15,
+  currentPage: 1,
+  numberOfResults: 0
 })
 
 export const mutations = {
   SET_USERS(state, users) {
     state.users = users
+  },
+
+  SET_NUMBER_OF_TOTAL_RESULTS(state, numberOfResults) {
+    state.numberOfResults = numberOfResults
   }
 }
 
 export const actions = {
-  search({ commit }, { simpleSearchTerm }) {
+  search({ commit, state: { resultsPerPage, currentPage } }, { simpleSearchTerm }) {
     return this.$axios.get('/api/users', {
       params: {
-        q: simpleSearchTerm
+        q: simpleSearchTerm,
+        per_page: resultsPerPage,
+        page: currentPage
       }
     }).then(res => {
-      const { users } = res.data
+      // eslint-disable-next-line camelcase
+      const { total_count, items } = res.data
 
-      commit('SET_USERS', users)
-      return users
+      commit('SET_USERS', items)
+      commit('SET_NUMBER_OF_TOTAL_RESULTS', total_count)
+      return items
     })
   }
 }
--- store/users.spec.js
+++ store/users.spec.js
@@ -7,10 +7,28 @@ describe('state', () => {
     defaultState = state()
   })
 
-  it('should have an empty users arry on the default state', () => {
+  it('should have an empty users array on the default state', () => {
     const { users } = defaultState
 
     expect(users).toEqual([])
+  })
+
+  it('should have a default number of user search results per page on the default state', () => {
+    const { resultsPerPage } = defaultState
+
+    expect(resultsPerPage).toBe(15)
+  })
+
+  it('should have the current page of user search results on the default state', () => {
+    const { currentPage } = defaultState
+
+    expect(currentPage).toBe(1)
+  })
+
+  it('should have the total number of user search results on the default state', () => {
+    const { numberOfResults } = defaultState
+
+    expect(numberOfResults).toBe(0)
   })
 })
 
@@ -28,6 +46,22 @@ describe('mutations', () => {
 
     it('should assign the returned users to the users on the users on the state', () => {
       expect(currentState.users).toEqual(users)
+    })
+  })
+
+  describe('SET_NUMBER_OF_TOTAL_RESULTS', () => {
+    let currentState
+    let numberOfResults
+
+    beforeAll(() => {
+      currentState = { numberOfResults: 0 }
+      numberOfResults = 30
+
+      mutations.SET_NUMBER_OF_TOTAL_RESULTS(currentState, numberOfResults)
+    })
+
+    it('should assign the number of user search results to the number of results on the state', () => {
+      expect(currentState.numberOfResults).toEqual(numberOfResults)
     })
   })
 })
@@ -54,14 +88,26 @@ describe('actions', () => {
   describe('when a successful search action call is made', () => {
     let simpleSearchTerm
     let mockReturnedUsers
+    let mockNumOfTotalResults
+    let currentState
 
     beforeAll(async () => {
       simpleSearchTerm = 'test'
       mockReturnedUsers = []
+      currentState = {
+        resultsPerPage: 15,
+        currentPage: 1
+      }
+      mockNumOfTotalResults = 30
 
-      $axios.get = jest.fn().mockResolvedValue({ data: { users: mockReturnedUsers } })
+      $axios.get = jest.fn().mockResolvedValue({
+        data: {
+          total_count: mockNumOfTotalResults,
+          items: mockReturnedUsers
+        }
+      })
 
-      await actions.search({ commit }, { simpleSearchTerm })
+      await actions.search({ commit, state: currentState }, { simpleSearchTerm })
     })
 
     afterAll(() => {
@@ -72,13 +118,19 @@ describe('actions', () => {
     it('should make a get request to get users using the simple search term', () => {
       expect($axios.get).toHaveBeenCalledWith('/api/users', expect.objectContaining({
         params: {
-          q: simpleSearchTerm
+          q: simpleSearchTerm,
+          per_page: currentState.resultsPerPage,
+          page: currentState.currentPage
         }
       }))
     })
 
     it('should perform the SET_USERS mutation with the search results', () => {
       expect(commit).toHaveBeenCalledWith('SET_USERS', mockReturnedUsers)
+    })
+
+    it('should perform the SET_NUMBER_OF_TOTAL_RESULTS mutation with the number of total returned search results', () => {
+      expect(commit).toHaveBeenCalledWith('SET_NUMBER_OF_TOTAL_RESULTS', mockNumOfTotalResults)
     })
   })
 })
