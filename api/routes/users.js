7119e769c6b9e4282ad46545fff68a283960529f
Keionne Derousselle <knnderousselle@gmail.com>
2021-03-26 17:48:52 -0600

    fixing bug in user search route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,30 @@
 const { Router } = require('express')
+const { KD_GITHUB_SEARCH_ACCESS_TOKEN } = process.env
+const { Octokit } = require('@octokit/rest')
 const router = Router()
 
-router.get('/', (req, res, next) => {
-  res.json({ status: req.query.q })
+router.get('/', async (req, res) => {
+  const appOctokit = new Octokit({
+    auth: KD_GITHUB_SEARCH_ACCESS_TOKEN
+  })
+
+  // TODO: sanitize query and make sure it is validated
+
+  try {
+    // eslint-disable-next-line camelcase
+    const { q, per_page, page } = req.query
+    const { data } = await appOctokit.search.users({ q, per_page, page })
+
+    res.json(data)
+  } catch (e) {
+    res
+      .status(e.status || 500)
+      .json({
+        message: e.message || 'Unknown server error occurred!',
+        status: e.status || 500,
+        stack: e.stack
+      })
+  }
 })
 
 module.exports = router
