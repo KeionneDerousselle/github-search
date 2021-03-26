ba5ddf6363e8e1edfe18786767c88d607521e9af
Keionne Derousselle <knnderousselle@gmail.com>
2021-03-25 23:34:58 -0600

    adding a new users route to the api

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require('express')
 const router = Router()
 
 const healthRoutes = require('./health')
+const usersRoutes = require('./users')
 
 router.use('/health', healthRoutes)
+router.use('/users', usersRoutes)
 
 router.get('/', (req, res, next) => {
   res.json({ message: 'success' })
