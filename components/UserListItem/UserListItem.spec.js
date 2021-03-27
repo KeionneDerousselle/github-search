afea75468f00d1dff3a26d1068ece6efd1980123
Keionne Derousselle <knnderousselle@gmail.com>
2021-03-26 21:43:43 -0600

    adding a user list item component

---
+++ components/UserListItem/UserListItem.spec.js
@@ -0,0 +1,50 @@
+import UserListItem from '.'
+
+describe('User List Item', () => {
+  let wrapper
+  let user
+
+  beforeAll(() => {
+    user = {
+      name: 'Test',
+      login: 'test_user',
+      avatar_url: 'https://some.fake.com/avatar.png'
+    }
+
+    wrapper = shallowPreMocked(UserListItem, {
+      propsData: {
+        user
+      }
+    })
+  })
+
+  afterAll(() => {
+    wrapper.destroy()
+  })
+
+  it('should render as expected', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render the user\'s avatar', () => {
+    const avatar = wrapper.get('.user-list-item__avatar')
+
+    expect(avatar.element).toBeVisible()
+    expect(avatar.attributes().src).toBe(user.avatar_url)
+    expect(avatar.attributes().alt).toBe(`Profile picture for ${user.login}`)
+  })
+
+  it('should render the user\'s usernane', () => {
+    const username = wrapper.get('.user-list-item__details__login')
+
+    expect(username.element).toBeVisible()
+    expect(username.text()).toBe(user.login)
+  })
+
+  it('should render the user\'s name', () => {
+    const name = wrapper.get('.user-list-item__details__name')
+
+    expect(name.element).toBeVisible()
+    expect(name.text()).toBe(user.name)
+  })
+})
---
+++ components/UserListItem/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <li
+    v-bind="$attrs"
+    class="user-list-item"
+    v-on="$listeners">
+    <img
+      :src="user.avatar_url"
+      :alt="`Profile picture for ${user.login}`"
+      class="user-list-item__avatar">
+    <div class="user-list-item__details">
+      <span class="user-list-item__details__name">Test</span>
+      <span class="user-list-item__details__login">{{ user.login }}</span>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style lang="scss">
+.user-list-item {
+  @apply list-none flex items-center pt-4 pb-4 pl-6 pr-6;
+
+  &:not(:last-of-type) {
+    @apply border-b border-gray-300;
+  }
+
+  &:hover {
+    @apply bg-indigo-50;
+  }
+
+  &__avatar {
+    @apply rounded-full w-10 h-10 mr-2.5;
+  }
+
+  &__details {
+    &__name {
+      @apply text-lg text-gray-800 block;
+    }
+
+    &__login {
+      @apply text-sm text-gray-600 block;
+    }
+  }
+}
+</style>
